0d86341e537db71c4192ec25b014b19b866a9ac4
Govind <govind.sh@browserstack.com>
2025-01-06 20:41:18 +0530

    Made changes according to feedback provided.

--- solution1.sh
+++ solution1.sh
@@ -6,7 +6,12 @@ if [ -z "$1" ]; then
 fi
 
 #setting the logfile name
-LOGFILE=$1
+FILE=$1
 
 #The command to get the longest running query
-grep 'Completed.*in [0-9]*ms' ./logfile.log | awk -F'in ' '{print $2, $0}' | sort -nr | head -1 | cut -d' ' -f2- 
+sed -n 's/.*ted.*in \([0-9]*ms\).*/\1/p' "$FILE"|sort -nr|head -1|xargs -I{} grep -B1 {} "$FILE" 
+
+
+
+
+
